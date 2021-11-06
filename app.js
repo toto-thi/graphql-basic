df96237db39508f25f3163b61e264745b2edf0a8
Toto <66200832+toto-thi@users.noreply.github.com>
2021-11-06 19:36:47 +0700

    minor update

--- app.js
+++ app.js
@@ -1,33 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlHTTP } = require('express-graphql');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
 
-const graphQLSchema = require('./graphql/schema/index');
-const graphQLResolvers = require('./graphql/resolvers/index');
-const isAuth = require('./middleware/is-auth');
+const graphQLSchema = require("./graphql/schema/index");
+const graphQLResolvers = require("./graphql/resolvers/index");
+const isAuth = require("./middleware/is-auth");
 
-
-require('dotenv').config();
-
+require("dotenv").config();
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type",
+    "Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(bodyParser.json());
 
 app.use(isAuth);
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: graphQLSchema,
     rootValue: graphQLResolvers,
-    graphiql: true
-})
+    graphiql: true,
+  })
 );
 
-mongoose.connect(`
+mongoose
+  .connect(
+    `
 mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}
-@cluster0.qjewy.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`).then( () => {
-    app.listen(3000);
-}).catch(err => {
+@cluster0.qjewy.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(8000);
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
